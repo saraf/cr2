445e9344d1da24c17c19cf033b5bba827ed9b5e8
Aalhad Saraf <aalhad@gmail.com>
2017-08-04 23:27:59 +0530

    First working version on rhcloud

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -10,7 +10,8 @@ module.exports = {
   port: process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080,
 
   sequelize: {
-    uri: 'postgres://' + process.env.OPENSHIFT_POSTGRESQL_DB_USERNAME + ':' + process.env.OPENSHIFT_POSTGRESQL_DB_PASSWORD + '@' + process.env.OPENSHIFT_POSTGRESQL_DB_HOST + ':' + process.env.OPENSHIFT_POSTGRESQL_DB_PORT + '/cr2',
+    uri:  'postgres://'+ process.env.OPENSHIFT_POSTGRESQL_DB_USERNAME + 
+          ':' + process.env.OPENSHIFT_POSTGRESQL_DB_PASSWORD + '@' + process.env.OPENSHIFT_POSTGRESQL_DB_HOST+':' + process.env.OPENSHIFT_POSTGRESQL_DB_PORT + '/cr2', 
     options: {
       logging: false,
       dialect: 'postgres',
