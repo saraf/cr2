6ab2b5371b745044bd706d75c920795445e8e505
Aalhad Saraf <aalhad@gmail.com>
2017-08-04 22:48:30 +0530

    Initial commit

---
+++ server/api/task/index.spec.js
@@ -0,0 +1,79 @@
+'use strict';
+
+var proxyquire = require('proxyquire').noPreserveCache();
+
+var taskCtrlStub = {
+  index: 'taskCtrl.index',
+  show: 'taskCtrl.show',
+  create: 'taskCtrl.create',
+  update: 'taskCtrl.update',
+  destroy: 'taskCtrl.destroy'
+};
+
+var routerStub = {
+  get: sinon.spy(),
+  put: sinon.spy(),
+  patch: sinon.spy(),
+  post: sinon.spy(),
+  'delete': sinon.spy()
+};
+
+// require the index with our stubbed out modules
+var taskIndex = proxyquire('./index.js', {
+  'express': {
+    Router: function Router() {
+      return routerStub;
+    }
+  },
+  './task.controller': taskCtrlStub
+});
+
+describe('Task API Router:', function () {
+
+  it('should return an express router instance', function () {
+    expect(taskIndex).to.equal(routerStub);
+  });
+
+  describe('GET /api/tasks', function () {
+
+    it('should route to task.controller.index', function () {
+      expect(routerStub.get.withArgs('/', 'taskCtrl.index')).to.have.been.calledOnce;
+    });
+  });
+
+  describe('GET /api/tasks/:id', function () {
+
+    it('should route to task.controller.show', function () {
+      expect(routerStub.get.withArgs('/:id', 'taskCtrl.show')).to.have.been.calledOnce;
+    });
+  });
+
+  describe('POST /api/tasks', function () {
+
+    it('should route to task.controller.create', function () {
+      expect(routerStub.post.withArgs('/', 'taskCtrl.create')).to.have.been.calledOnce;
+    });
+  });
+
+  describe('PUT /api/tasks/:id', function () {
+
+    it('should route to task.controller.update', function () {
+      expect(routerStub.put.withArgs('/:id', 'taskCtrl.update')).to.have.been.calledOnce;
+    });
+  });
+
+  describe('PATCH /api/tasks/:id', function () {
+
+    it('should route to task.controller.update', function () {
+      expect(routerStub.patch.withArgs('/:id', 'taskCtrl.update')).to.have.been.calledOnce;
+    });
+  });
+
+  describe('DELETE /api/tasks/:id', function () {
+
+    it('should route to task.controller.destroy', function () {
+      expect(routerStub['delete'].withArgs('/:id', 'taskCtrl.destroy')).to.have.been.calledOnce;
+    });
+  });
+});
+//# sourceMappingURL=index.spec.js.map
---
+++ server/api/task/task.model.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define('Task', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    github_id: DataTypes.TEXT,
+    task_summary: DataTypes.TEXT,
+    task_description: DataTypes.TEXT,
+    summary_word_for_subtask1: DataTypes.TEXT,
+    summary_word_for_subtask2: DataTypes.TEXT,
+    is_commercial: DataTypes.TEXT,
+    task_status: DataTypes.TEXT,
+    assigned_to: DataTypes.TEXT,
+    broken_down_by: DataTypes.TEXT,
+    parent_task_id: DataTypes.TEXT,
+    child_task_ids: DataTypes.TEXT,
+    review_comments: DataTypes.TEXT,
+    codebase_reuse_SPOC_comments: DataTypes.TEXT
+  }, {
+    timestamps: true,
+    paranoid: true,
+    underscored: true,
+    freezeTableName: true,
+    tableName: 'tasks'
+
+  });
+};
+//# sourceMappingURL=task.model.js.map
